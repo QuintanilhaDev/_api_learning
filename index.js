eed12695216b20a7395bbfd2e900cfdbb484c17e
QuintanilhaDev <mateusthequintanilha@gmail.com>
2023-01-06 12:23:53 -0300

    The true versio again...

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const userRoute = require('./routes/_user_routes')
 const app = express()
-const port = 8080
+const port = 3030
 
 // userRoute(app)
 
 app.get('/', (req, res) => res.send("Olá mundo!"))
 
 app.listen(port, () => {
-    console.log('API rodando na porta 8080')
+    console.log('API rodando na porta 3030')
 })
